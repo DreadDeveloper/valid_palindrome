994650cd8a431fc8f6be53dce796f5e7d9b34083
Abhinav Kumar <kumaraka2000@gmail.com>
2023-04-18 18:46:21 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Solution solution = new Solution();
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(solution.isPalindrome(s));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Solution {
+public class Solution
+{
+    public boolean isPalindrome(String s)
+    {
+        if(s.isEmpty())
+        {
+            return true;
+        }
+        int i, j;
+        for(i = 0, j = s.length() - 1; i < j;)
+        {
+            if(!Character.isLetterOrDigit(s.charAt(i)))
+            {
+                i++;
+            }
+            else if(!Character.isLetterOrDigit(s.charAt(j)))
+            {
+                j--;
+            }
+            else
+            {
+                if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+                {
+                    return false;
+                }
+                i++; j--;
+            }
+        }
+        return true;
+    }
 }
